0487953295af99f22cab34afd66ceb84fec3d87a
Tom Naessens <Tom.Naessens@UGent.be>
2014-12-08 14:42:55 +0100

    Make it faster (about 10000%)

--- tree-of-life/calc_lca.py
+++ tree-of-life/calc_lca.py
@@ -7,95 +7,13 @@ from itertools import zip_longest
 from tree_of_life import get_tree, CLASSES
 
 TREE = get_tree()
-MATCHES = []
+LCAS = []
 UNFOUND = set()
 UNMATCHED = set()
 CORRECT = 0
-COUNTER = 0
 
-
-def get_lca(lineages):
-    """Does the actual LCA calculation"""
-
-    # Use -1 as fillvalue here, we'll filter it out later
-    for i, taxons in enumerate(zip_longest(*lineages, fillvalue=-1)):
-        # Remove the filling and the invalid taxons
-        taxons_filtered = [t for t in taxons if t == -1 or t is None or t.valid_taxon]
-        taxon_set = set(taxons_filtered) - set([-1])
-
-        if CLASSES[i] == 'genus' or CLASSES[i] == 'species':
-            taxon_set = taxon_set - set([None])
-
-        if len(taxon_set) == 1:
-            val = taxon_set.pop()
-            if val:
-                lca = val
-        elif len(taxon_set) > 1:
-            return lca
-
-    return lca
-
-
-def handle_lca(line):
-    """Gets the lineages from the tree and performs the LCA calculation"""
-    lineages = []
-    lca = None
-
-    # Get the prots from Unipept
-    prot_result = subprocess.Popen(
-        "unipept pept2prot -s taxon_id {}".format(line),
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
-    )
-
-    # Get the lineage for each prot
-    for prot in prot_result.stdout.readlines()[1:]:
-        taxon = TREE.taxons[int(prot)]
-        lineage = taxon.get_lineage(no_rank=False, invalid=False)
-        lineages.append(lineage)
-
-    for lineage in lineages:
-        print([taxon.taxon_id if taxon else 0 for taxon in lineage])
-
-    # If we have a result, get the LCA, otherwise, add it to the unfound ones
-    if lineages:
-        lca = get_lca(lineages).taxon_id
-        print("LCA: {}: {}".format(lca, TREE.taxons[lca].name))
-    else:
-        UNFOUND.add(line)
-        print("LCA: No LCA found for {}".format(line))
-
-    return lca, lineages
-
-
-def get_unipept_lca(line):
-    """Gets the Unipept LCA"""
-
-    unipept_lca = None
-
-    # Get the LCA from Unipept
-    result = subprocess.Popen(
-        "unipept pept2lca -s taxon_id -s taxon_name {}".format(line),
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
-    )
-
-    unipept_result = result.stdout.readlines()
-
-    # Hande the result
-    if len(unipept_result) > 1:
-        unipept_lca = unipept_result[1].decode('utf-8').split(',')
-        print("Unipept LCA: {}: {}".format(unipept_lca[0], unipept_lca[1]))
-    else:
-        print("Unipept LCA: None LCA found for {}".format(line))
-
-    return unipept_lca
-
-
-def print_lcas(filename):
-    lineages = [TREE.taxons[prot].get_lineage(no_rank=False, invalid=False) for prot in MATCHES if prot]
+def print_tree_json(filename):
+    lineages = [TREE.taxons[prot].get_lineage(no_rank=False, invalid=False) for prot in LCAS if prot]
 
     # Remove all the children from the taxons
     for lineage in lineages:
@@ -116,7 +34,7 @@ def print_lcas(filename):
                 valid_parent.children.add(taxon)
 
             # Also set the self_count
-            taxon.self_count = MATCHES.count(taxon.taxon_id)
+            taxon.self_count = LCAS.count(taxon.taxon_id)
 
     # Percolate the counts up the tree
     lineages[0][0].add_counts()
@@ -126,27 +44,119 @@ def print_lcas(filename):
         lineages[0][0].to_json(f)
 
 
-for line in sys.stdin:
-    print("Calculating LCA for {}".format(line))
+class Peptide:
+    def __init__(self, pept):
+        self.pept = pept
+        self.prots = []
+        self.lca = None
+        self.unipept_lca = None
+        self.lineages = []
 
-    lca, lineages = handle_lca(line)
-    unipept_lca = get_unipept_lca(line)
 
-    if (not lineages and not unipept_lca) or \
-            (lineages and unipept_lca and lca == int(unipept_lca[0])):
+    def addprot(self, prot):
+        self.prots.append(prot)
+
+
+    def handle_lca(self):
+        """Gets the lineages from the tree and performs the LCA calculation"""
+
+        # Get the lineage for each prot
+        for prot in self.prots:
+            taxon = TREE.taxons[int(prot)]
+            lineage = taxon.get_lineage(no_rank=False, invalid=False)
+            self.lineages.append(lineage)
+
+        # If we have a result, get the LCA, otherwise, add it to the unfound ones
+        if self.lineages:
+            self.lca = self.get_lca().taxon_id
+        else:
+            UNFOUND.add(self.pept)
+
+
+    def get_lca(self):
+        """Does the actual LCA calculation"""
+
+        # Use -1 as fillvalue here, we'll filter it out later
+        for i, taxons in enumerate(zip_longest(*self.lineages, fillvalue=-1)):
+            # Remove the filling and the invalid taxons
+            taxons_filtered = [t for t in taxons if t == -1 or t is None or t.valid_taxon]
+            taxon_set = set(taxons_filtered) - set([-1])
+
+            if CLASSES[i] == 'genus' or CLASSES[i] == 'species':
+                taxon_set = taxon_set - set([None])
+
+            if len(taxon_set) == 1:
+                val = taxon_set.pop()
+                if val:
+                    lca = val
+            elif len(taxon_set) > 1:
+                return lca
+
+        return lca
+
+
+fastafile = "/tmp/fasta.tmp"
+# Dict containing the input sequence
+inputarray = []
+# Dict containing the peptides to their objects
+pept2prot = dict()
+
+# Create a temp fastafile for easy Unipept querying
+with open(fastafile, "wb") as f:
+    for i, line in enumerate(sys.stdin):
+        f.write(">|{}\n".format(i).encode('utf-8'))
+        f.write(line.encode('utf-8'))
+
+        line = line.strip()
+
+        inputarray.append(line)
+
+        if line not in pept2prot:
+            pept2prot[line] = Peptide(line.strip())
+
+
+# Get all the proteins
+prot_result = subprocess.Popen(
+    "unipept pept2prot -i {} -s taxon_id -s peptide".format(fastafile),
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT
+)
+
+for prot in prot_result.stdout.readlines()[1:]:
+    _, pept, prot = prot.decode('utf-8').strip().split(',')
+    pept2prot[pept].addprot(prot)
+
+
+# Get unipept LCAs
+unipept_lca_result = subprocess.Popen(
+    "unipept pept2lca -i {} -s taxon_id -s peptide".format(fastafile),
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT
+)
+
+for lca in unipept_lca_result.stdout.readlines()[1:]:
+    _, pept, lca = lca.decode('utf-8').strip().split(',')
+    pept2prot[pept].unipept_lca = int(lca)
+
+# Get all the LCAs
+for pept in pept2prot.values():
+    pept.handle_lca()
+
+
+for pept in inputarray:
+    pept = pept2prot[pept]
+
+    if pept.lca == pept.unipept_lca:
         CORRECT += 1
-        MATCHES.append(lca)
+        LCAS.append(pept.lca)
     else:
-        UNMATCHED.add(line)
+        UNMATCHED.add(pept.pept)
 
-    COUNTER += 1
-    print()
-    print("=================== {}% ==================".format(COUNTER/3983*100))
-    print("Unmatched: {}, {}".format(len(UNMATCHED), ', '.join(UNMATCHED)))
-    print("Correct: {}, total: {}, accuracy: {}%".format(CORRECT, COUNTER, CORRECT/COUNTER*100))
-    print("=================== {}% ==================".format(COUNTER/3983*100))
-    print()
-
+print("Unmatched: {}, {}".format(len(UNMATCHED), ', '.join(UNMATCHED)))
 print("Unfound: {}, {}".format(len(UNFOUND), ', '.join(UNFOUND)))
+print()
+print("Correct: {}, total: {}, accuracy: {}%".format(CORRECT, len(inputarray), CORRECT/len(inputarray)*100))
 
-print_lcas("data/lca_result.json")
+print_tree_json("data/lca_result.json")
