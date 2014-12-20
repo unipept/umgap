b172154405e4113b7d85e93ffa233d854a03622b
Tom Naessens <Tom.Naessens@UGent.be>
2014-12-20 20:33:39 +0100

    Moved the utility functions out of calc_lca.py

--- tree-of-life/calc_lca.py
+++ tree-of-life/calc_lca.py
@@ -3,45 +3,14 @@
 import sys
 import subprocess
 from itertools import zip_longest
+from collections import OrderedDict
 
 from tree_of_life import get_tree, CLASSES
+from utils import print_tree_json, compare_to_unipept
 
 TREE = get_tree()
 LCAS = []
 UNFOUND = set()
-UNMATCHED = set()
-CORRECT = 0
-
-def print_tree_json(filename):
-    lineages = [TREE.taxons[prot].get_lineage(no_rank=False, invalid=False) for prot in LCAS if prot]
-
-    # Remove all the children from the taxons
-    for lineage in lineages:
-        for taxon in lineage:
-            if taxon:
-                taxon.children = set()
-                taxon.count = 0
-                taxon.self_count = 0
-
-    # Add the children from the found lineages back
-    for taxons in zip_longest(*lineages, fillvalue=-1):
-        taxons_set = set(taxons) - set([None]) - set([-1])
-
-        # Add the correct children
-        for taxon in taxons_set:
-            valid_parent = taxon.get_valid_parent()
-            if taxon.parent_id != taxon.taxon_id:
-                valid_parent.children.add(taxon)
-
-            # Also set the self_count
-            taxon.self_count = LCAS.count(taxon.taxon_id)
-
-    # Percolate the counts up the tree
-    lineages[0][0].add_counts()
-
-    # Print the result
-    with open(filename, "wb") as f:
-        lineages[0][0].to_json(f)
 
 
 class Peptide:
@@ -60,8 +29,6 @@ class Peptide:
     def handle_lca(self):
         """Gets the lineages from the tree and performs the LCA calculation"""
 
-        print("Calculating the LCA for {}".format(self.pept))
-
         # Get the lineage for each prot
         for prot in self.prots:
             taxon = TREE.taxons[int(prot)]
@@ -75,8 +42,12 @@ class Peptide:
         # If we have a result, get the LCA, otherwise, add it to the unfound ones
         if self.lineages:
             self.lca = self.get_lca().taxon_id
+            LCAS.append(self.lca)
+            print("LCA for {} is {} ({})".format(self.pept, TREE.taxons[self.lca].name, self.lca))
         else:
             UNFOUND.add(self.pept)
+            print("LCA for {} not found".format(self.pept))
+
 
 
     def get_lca(self):
@@ -105,7 +76,7 @@ fastafile = "/tmp/fasta.tmp"
 # Dict containing the input sequence
 inputarray = []
 # Dict containing the peptides to their objects
-pept2prot = dict()
+pept2prot = OrderedDict()
 
 # Create a temp fastafile for easy Unipept querying
 with open(fastafile, "wb") as f:
@@ -133,37 +104,12 @@ for prot in prot_result.stdout.readlines()[1:]:
     _, pept, prot = prot.decode('utf-8').strip().split(',')
     pept2prot[pept].addprot(prot)
 
-
-# Get unipept LCAs
-unipept_lca_result = subprocess.Popen(
-    "unipept pept2lca -i {} -s taxon_id -s peptide".format(fastafile),
-    shell=True,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT
-)
-
-for lca in unipept_lca_result.stdout.readlines()[1:]:
-    _, pept, lca = lca.decode('utf-8').strip().split(',')
-    pept2prot[pept].unipept_lca = int(lca)
-
 # Get all the LCAs
 for pept in pept2prot.values():
     pept.handle_lca()
 
+#if UNFOUND:
+#    print("Unfound: {}, {}".format(len(UNFOUND), ', '.join(UNFOUND)))
 
-for pept in inputarray:
-    pept = pept2prot[pept]
-
-    if pept.lca == pept.unipept_lca:
-        CORRECT += 1
-        LCAS.append(pept.lca)
-    else:
-        print("Unmatched for {}: {} ({}) vs {} ({})".format(pept.pept, TREE.taxons[pept.lca].name, pept.lca, TREE.taxons[pept.unipept_lca].name, pept.unipept_lca))
-        UNMATCHED.add(pept.pept)
-
-print("Unmatched: {}, {}".format(len(UNMATCHED), ', '.join(UNMATCHED)))
-print("Unfound: {}, {}".format(len(UNFOUND), ', '.join(UNFOUND)))
-print()
-print("Correct: {}, total: {}, accuracy: {}%".format(CORRECT, len(inputarray), CORRECT/len(inputarray)*100))
-
-print_tree_json("data/lca_result.json")
+#compare_to_unipept(fastafile, pept2prot, TREE, inputarray)
+#print_tree_json("data/lca_result.json", TREE, LCAS)
