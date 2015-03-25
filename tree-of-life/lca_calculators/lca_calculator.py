b8d95f6fa55967772e85d934e8627d9c01dd82cf
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-25 10:36:52 +0100

    Add time elapsed to timings

--- tree-of-life/lca_calculators/lca_calculator.py
+++ tree-of-life/lca_calculators/lca_calculator.py
@@ -7,7 +7,8 @@ from lca_calculators.tree_of_life import get_tree
 class LCA_Calculator:
     """Provides methods to perform LCA calculations"""
     def __init__(self):
-        print("Building tree. Time: {}".format(time.time()), file=sys.stderr)
+        starttime = time.time()
+        print("Building tree. Time: {}".format(starttime), file=sys.stderr)
         self.tree = get_tree()
-        print("Tree built. Time: {}".format(time.time()), file=sys.stderr)
+        print("Tree built. Time: {}, time elapsed: {}".format(time.time(), time.time()-starttime), file=sys.stderr)
         print("---", file=sys.stderr)
