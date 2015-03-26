5674a27000d22a9864afc173a7338cb5a3bad039
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-26 12:54:33 +0100

    Remove obsolete files

--- tree-of-life/entrez/split_unaggregated_file.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-awk '/>\|/ { close("results/fastafiles/"header".fst"); header = $0; sub(/>\|/, "", header); file = "results/fastafiles/"header".fst"; print $0 > file } !/>\|/ { print $0 >> file}'
