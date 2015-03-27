07ca1597489c71d0cd2e80ad7a8664d6bcd7fb98
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-27 16:15:07 +0100

    Rewrite pept2prot2filter2lca completely to be more pipes&filters-y

--- tree-of-life/pept2prot2filter2lca.py
+++ tree-of-life/pept2prot2filter2lca.py
@@ -1,37 +1,71 @@
+""" Given a file of pept2prot2filter entries, gets the LCA for every peptide.
+
+Expects a list grouped first by the FASTA header and then by peptide for speed reasons
+Not necessarily ordered, just grouped is fine
+"""
+
 import sys
 
 from lca_calculators.tree_based_lca import Tree_LCA_Calculator
 
-class FastaRecord:
-    def __init__(self, fasta_id):
-        self.fasta_id = fasta_id
-        self.peptides = dict()
 
-    def add_peptide(self, peptide, taxon_id):
-        if peptide not in self.peptides:
-            self.peptides[peptide] = []
-
-        self.peptides[peptide].append(int(taxon_id))
-
-
-fasta2record = dict()
+# Skip the header
 next(sys.stdin)
-for line in sys.stdin:
-    fasta_header, peptide, _, taxon_id = line.strip().split(',')
-
-    fasta_id = fasta_header[1:]
-
-    if fasta_id not in fasta2record:
-        fasta2record[fasta_id] = FastaRecord(fasta_id)
-
-    fasta2record[fasta_id].add_peptide(peptide, taxon_id)
 
 calculator = Tree_LCA_Calculator()
 print("fasta_header,taxon_id,taxon_name,taxon_rank")
-for fasta_record in fasta2record.values():
 
-    lcas = [calculator.calc_lca(fasta_record.peptides[peptide]) for peptide in fasta_record.peptides]
-    lca = calculator.calc_lca(lcas)
+# Initialize variables
+current_fasta_id = ''
+current_peptide = ''
+taxon_ids = set()
+lcas = set()
 
-    taxon = calculator.tree.taxons[lca]
-    print(">{},{},{},{}".format(fasta_record.fasta_id, taxon.taxon_id, taxon.name, taxon.rank))
+
+# Iterate over the lines
+for line in sys.stdin:
+
+    fasta_header, peptide, _, taxon_id = line.strip().split(',')
+    fasta_id = fasta_header[1:]
+
+    # New peptide: calculate the set of current peptides and add the lca to the fasta_set
+    # When there is a new fasta header, we also want to do this
+    if (current_peptide and current_peptide != peptide) or (current_fasta_id and current_fasta_id != fasta_id):
+        # Calculate the lca of the taxon_ids for the current peptide
+        lca = calculator.calc_lca(taxon_ids)
+
+        # Add it to the lcas for the current fasta id
+        lcas.add(lca)
+
+        # Empty the taxon_ids set
+        taxon_ids = set()
+
+    # New fasta header: calculate the LCA of the peptides so far and print it out
+    if current_fasta_id and current_fasta_id != fasta_id:
+        # Calculate the lca of the lcas for the current peptide
+        lca = calculator.calc_lca(lcas)
+
+        # Get the taxon we're talking about
+        taxon = calculator.tree.taxons[lca]
+        print(">{},{},{},{}".format(current_fasta_id, taxon.taxon_id, taxon.name, taxon.rank))
+
+        # Empty the sets for the current fasta header and taxon_ids
+        lcas = set()
+
+    # Update the current values
+    current_fasta_id = fasta_id
+    current_peptide = peptide
+
+    # Add the taxon_id to the set
+    taxon_ids.add(int(taxon_id))
+
+
+# And calculate it once again for the last sets
+lca = calculator.calc_lca(taxon_ids)
+lcas.add(lca)
+lca = calculator.calc_lca(lcas)
+taxon = calculator.tree.taxons[lca]
+print(">{},{},{},{}".format(current_fasta_id, taxon.taxon_id, taxon.name, taxon.rank))
+
+# Finally clean the calculator
+calculator.cleanup()
