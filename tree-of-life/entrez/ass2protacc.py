8e6ba2f19f37f89b1ca2da6ecdad4560f73ff326
Tom Naessens <Tom.Naessens@UGent.be>
2015-04-01 13:24:51 +0200

    Completely rewrite entrez scripts for PEP8 and general codestyle

--- tree-of-life/entrez/ass2protacc.py
+++ tree-of-life/entrez/ass2protacc.py
@@ -1,55 +1,25 @@
-import subprocess
-import urllib.request, urllib.parse, urllib.error
-import os
+"""Given a refseq assembly accession ID, return a list of accession ids
+from all the proteins in the assembly"""
+
 import sys
 
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio import SeqIO
 from Bio import Entrez
 
-
-# Variables
-Entrez.email = "tom.naessens@ugent.be"
-
-def get_nucleotide_ids(assembly_id):
-    """Get all the nucleotides for a projectid"""
-
-    url = "ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/All/{}.assembly.txt".format(assembly_id)
-
-    f = urllib.request.urlretrieve(url, 'assembly.tmp')
-
-    awk_process = subprocess.Popen(
-        "cat assembly.tmp | awk '!/^#/ {print $5}'",
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
-    )
-    data = awk_process.stdout.readlines()
-    data = [dat.decode('utf-8').strip() for dat in data]
-
-    os.remove("assembly.tmp")
-
-    return data
+from utils import get_nucleotide_ids, get_nucleotide_features
 
 
-def get_nucleotide_features(nucleotide_id):
-    """Yields all the features of a speficic nucleotide"""
+if __name__ == '__main__':
+    # Variables
+    Entrez.email = "tom.naessens@ugent.be"
 
-    handle = Entrez.efetch(db="nucleotide", id=nucleotide_id, retmode="text", rettype="gbwithparts")
-    record = SeqIO.read(handle, "genbank")
-    handle.close()
-
-    yield record.features
+    if len(sys.argv) != 2:
+        print("Usage: {} [refseq_id]".format(sys.argv[0]))
+        exit(1)
 
 
-if len(sys.argv) != 2:
-    print("Usage: {} [refseq_id]".format(sys.argv[0]))
-    exit(1)
-
-# Get all the proteins
-for nucleotide_id in get_nucleotide_ids(sys.argv[1]):
-    for features in get_nucleotide_features(nucleotide_id):
-        for feature in features:
-            if feature.type == 'CDS':
-                print(feature.qualifiers['protein_id'][0])
+    # Get all the proteins
+    for nucleotide_id in get_nucleotide_ids(sys.argv[1]):
+        for features in get_nucleotide_features(nucleotide_id):
+            for feature in features:
+                if feature.type == 'CDS':
+                    print(feature.qualifiers['protein_id'][0])
