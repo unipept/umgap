d0f5a09086504c7538cd27eb9eeea77ac6335c7d
Tom Naessens <Tom.Naessens@UGent.be>
2015-04-01 13:37:12 +0200

    Get rid of the old lca2lca scripts

--- tree-of-life/pept2prot2filter2lca.py
+++ tree-of-life/pept2prot2filter2lca.py
@@ -1,71 +1,68 @@
-""" Given a file of pept2prot2filter entries, gets the LCA for every peptide.
+""" Given a file of pept2prot2filter entries, gets the LCA for every fasta header by
+getting the LCAs per peptides and by doing another LCA step on these results.
 
 Expects a list grouped first by the FASTA header and then by peptide for speed reasons
 Not necessarily ordered, just grouped is fine
 """
 
 import sys
+from itertools import groupby
+from operator import itemgetter
+
+import argparse
 
 from lca_calculators.tree_based_lca import Tree_LCA_Calculator
+
+# Input argument parsing
+parser = argparse.ArgumentParser(description='Calculate the LCAs for a given fastafile after pept2lca')
+parser.add_argument('-c', '--check-against', dest='reference_taxon_id', type=int, help='check against a taxon_id')
+args = parser.parse_args()
+
+
+calculator = Tree_LCA_Calculator()
+
+
+print("fasta_header,taxon_id,taxon_name,taxon_rank", end="")
+if args.reference_taxon_id:
+    print(",on_lineage")
+else:
+    print()
+
+
+def reduce_per_peptide(lines):
+    lca = calculator.calc_lca([int(line[3]) for line in lines])
+    return lca
+
+
+def reduce_per_fasta(lines):
+    peptide_groups = (g for k, g in groupby(lines, itemgetter(1)))
+
+    lcas_per_peptide = (reduce_per_peptide(group) for group in peptide_groups)
+
+    lca = calculator.calc_lca(lcas_per_peptide)
+    taxon = calculator.tree.taxons[lca]
+
+    return taxon
+
+
+def parse(input_):
+    splits = (line.rstrip().split(',') for line in input_)
+    fasta_groups = ((k, g) for k, g in groupby(splits, itemgetter(0)))
+    reduced_fastas = ((k, reduce_per_fasta(group)) for k, group in fasta_groups)
+
+    for fasta_header, taxon in reduced_fastas:
+        print("{},{},{},{}".format(fasta_header, taxon.taxon_id, taxon.name, taxon.rank), end="")
+
+        # We want to check against a reference taxon
+        if args.reference_taxon_id:
+            on_lineage = calculator.calc_lca([taxon.taxon_id, args.reference_taxon_id]) == args.reference_taxon_id
+            print(",{}".format(int(on_lineage)))
+        else:
+            print()
 
 
 # Skip the header
 next(sys.stdin)
+parse(sys.stdin)
 
-calculator = Tree_LCA_Calculator()
-print("fasta_header,taxon_id,taxon_name,taxon_rank")
-
-# Initialize variables
-current_fasta_id = ''
-current_peptide = ''
-taxon_ids = set()
-lcas = set()
-
-
-# Iterate over the lines
-for line in sys.stdin:
-
-    fasta_header, peptide, _, taxon_id = line.strip().split(',')
-    fasta_id = fasta_header[1:]
-
-    # New peptide: calculate the set of current peptides and add the lca to the fasta_set
-    # When there is a new fasta header, we also want to do this
-    if (current_peptide and current_peptide != peptide) or (current_fasta_id and current_fasta_id != fasta_id):
-        # Calculate the lca of the taxon_ids for the current peptide
-        lca = calculator.calc_lca(taxon_ids)
-
-        # Add it to the lcas for the current fasta id
-        lcas.add(lca)
-
-        # Empty the taxon_ids set
-        taxon_ids = set()
-
-    # New fasta header: calculate the LCA of the peptides so far and print it out
-    if current_fasta_id and current_fasta_id != fasta_id:
-        # Calculate the lca of the lcas for the current peptide
-        lca = calculator.calc_lca(lcas)
-
-        # Get the taxon we're talking about
-        taxon = calculator.tree.taxons[lca]
-        print(">{},{},{},{}".format(current_fasta_id, taxon.taxon_id, taxon.name, taxon.rank))
-
-        # Empty the sets for the current fasta header and taxon_ids
-        lcas = set()
-
-    # Update the current values
-    current_fasta_id = fasta_id
-    current_peptide = peptide
-
-    # Add the taxon_id to the set
-    taxon_ids.add(int(taxon_id))
-
-
-# And calculate it once again for the last sets
-lca = calculator.calc_lca(taxon_ids)
-lcas.add(lca)
-lca = calculator.calc_lca(lcas)
-taxon = calculator.tree.taxons[lca]
-print(">{},{},{},{}".format(current_fasta_id, taxon.taxon_id, taxon.name, taxon.rank))
-
-# Finally clean the calculator
 calculator.cleanup()
