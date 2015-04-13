239a1038aa0291966ed8a0f68dcd316a2f5fd756
Tom Naessens <Tom.Naessens@UGent.be>
2015-04-13 19:38:13 +0200

    Use ctypes instead of SWIG

--- tree-of-life/lca_calculators/tree_based_lca.py
+++ tree-of-life/lca_calculators/tree_based_lca.py
@@ -1,42 +1,101 @@
 from functools import reduce
+import os
 import sys
 import time
 
+import numpy
+import ctypes as ct
+
+from lca_calculators.tree_of_life import get_tree
 from lca_calculators.lca_calculator import LCA_Calculator
-import lca_calculators.vendor.rmq as rmq
+from lca_calculators.lca_calculator import DATA_DIR
+
+RMQLIB_PATH = os.path.join(os.path.dirname(__file__), 'vendor/librmq.o')
+
+class rmqinfo(ct.Structure):
+    _fields_ = [
+        ("alen", ct.c_int),
+        ("array", ct.POINTER(ct.c_int)),
+        ("sparse", ct.POINTER(ct.POINTER(ct.c_int))),
+        ("block_min", ct.POINTER(ct.c_int)),
+        ("labels", ct.POINTER(ct.c_int))
+    ]
 
 
 class Tree_LCA_Calculator(LCA_Calculator):
 
-    def __init__(self):
+    def __init__(self, serialize=True):
         super().__init__()
+
+        if os.path.isdir(DATA_DIR):
+            self.from_npy()
+        else:
+            print("Warning: no seralized data found. Building tree from taxons.tsv and serializing the result. Use serialize=False to not store the result to disk.", file=sys.stderr)
+
+            self.from_tree()
+            if serialize:
+                self.store_data()
+
+
+        self.librmq = ct.cdll.LoadLibrary(RMQLIB_PATH)
+        self.preprocess_rmq()
+
+
+    def store_data(self):
+        starttime = time.time()
+        print("Storing data to disk. Time: {}".format(starttime), file=sys.stderr)
+
+        os.makedirs(DATA_DIR)
+        numpy.save(os.path.join(DATA_DIR, "euler_tour.npy"), self.euler_tour)
+        numpy.save(os.path.join(DATA_DIR, "levels.npy"), self.levels)
+        numpy.save(os.path.join(DATA_DIR, "first_occurences.npy"), self.first_occurences)
+
+        print("Stored data to disk: {}, time elapsed: {}".format(time.time(), time.time()-starttime), file=sys.stderr)
+        print("---", file=sys.stderr)
+
+
+    def from_npy(self):
+        starttime = time.time()
+        print("Getting data from disk. Time: {}".format(starttime), file=sys.stderr)
+
+        self.euler_tour = numpy.load(os.path.join(DATA_DIR, "euler_tour.npy"))
+        self.levels = numpy.load(os.path.join(DATA_DIR, "levels.npy"))
+        self.first_occurences = numpy.load(os.path.join(DATA_DIR, "first_occurences.npy"))
+
+        print("Got data from disk: {}, time elapsed: {}".format(time.time(), time.time()-starttime), file=sys.stderr)
+        print("---", file=sys.stderr)
+
+
+    def from_tree(self):
+        self.tree = get_tree()
 
         starttime = time.time()
         print("Preprocessing LCA arrays. Time: {}".format(starttime), file=sys.stderr)
-        self.euler_tour =[None] * (2*self.tree.real_size - 1)
-        self.levels = [None] * (2*self.tree.real_size - 1)
-        self.first_occurences = [None] * self.tree.size
+
+        self.euler_tour = numpy.array([None] * (2*self.tree.real_size - 1))
+        self.levels = numpy.array([None] * (2*self.tree.real_size - 1))
+        self.first_occurences = numpy.array([None] * self.tree.size)
 
         self.dfs_run(self.tree.taxons[1], 0, 0)
 
-        print()
         print("Preprocessed LCA arrays: {}, time elapsed: {}".format(time.time(), time.time()-starttime), file=sys.stderr)
-        print("---", file=sys.stderr)
-
-        # Preprocess the RMQ
-        starttime = time.time()
-        print("Preprocessing RMQ. Time: {}".format(starttime), file=sys.stderr)
-        self.rmqinfo = self.preprocess_rmq()
-        print("Preprocessed RMQ. Time: {}, time elapsed: {}".format(time.time(), time.time()-starttime), file=sys.stderr)
         print("---", file=sys.stderr)
 
 
     def preprocess_rmq(self):
-        return rmq.rm_query_preprocess(self.levels, len(self.levels))
+        # Preprocess the RMQ
+        starttime = time.time()
+        print("Preprocessing RMQ. Time: {}".format(starttime), file=sys.stderr)
+
+        self.librmq.rm_query_preprocess.restype = ct.POINTER(rmqinfo)
+        self.rmqinfo = self.librmq.rm_query_preprocess(ct.c_void_p(self.levels.ctypes.data), len(self.levels))
+
+        print("Preprocessed RMQ. Time: {}, time elapsed: {}".format(time.time(), time.time()-starttime), file=sys.stderr)
+        print("---", file=sys.stderr)
 
 
     def free_rmqinfo(self):
-        rmq.rm_free(self.rmqinfo)
+        self.librmq.rm_free(self.rmqinfo)
 
 
     def dfs_run(self, taxon, iteration, level):
@@ -118,14 +177,14 @@ class Tree_LCA_Calculator(LCA_Calculator):
         except StopIteration:
             return None
 
-        level_of_higest_join, taxon_id = reduce(self._calc_lca_pair, taxon_ids, (level_of_highest_join, first))
+        level_of_highest_join, taxon_id = reduce(self._calc_lca_pair, taxon_ids, (level_of_highest_join, first))
         taxon = self.tree.taxons[taxon_id]
 
         return taxon.map_to_valid_taxon_id(allow_no_rank=allow_no_rank)
 
 
     def get_rmq(self, start, end):
-        return rmq.rm_query(self.rmqinfo, start, end)
+        return self.librmq.rm_query(self.rmqinfo, start, end)
 
 
     def cleanup(self):
