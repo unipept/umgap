e35ac530855350a77709066bba5a627705d7fddf
Tom Naessens <Tom.Naessens@UGent.be>
2015-05-06 13:23:55 +0200

    use arrayjobs for now

--- tree-of-life/benchmarking/analyse_genome.job.sh
+++ tree-of-life/benchmarking/analyse_genome.job.sh
@@ -8,6 +8,19 @@
 #PBS -m be
 #
 
+module load Ruby
+module load Python
+
+# make sure $PBS_ARRAYID is set, to avoid surprises
+if [ -z "$PBS_ARRAYID" ]
+then
+  echo 'ERROR: $PBS_ARRAYID is not set, submit job(s) using "qsub -t <array expression>"'
+  exit 1
+fi
+
 cd ..
+
+INPUTS_LIST_FILE=$HOME/metagenomics-scripts/tree-of-life/benchmarking/data/complete_assemblies.tsv
+INPUT_LINE=`sed -n "${PBS_ARRAYID}p" $INPUTS_LIST_FILE`
 
 ./benchmarking/analyse_genome.sh $1 -d $VSC_DATA -t $TMPDIR
