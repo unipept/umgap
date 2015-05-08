383cb4be519eaa3dc021a3036ad1678b6db8861d
Tom Naessens <Tom.Naessens@UGent.be>
2015-05-08 19:41:26 +0200

    Use the builtin entrez mapper, check for empty filter files

--- tree-of-life/entrez/seqacc2protid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read -r line
-do
-  without_version=${line%%.*}
-  url="http://www.uniprot.org/uniprot/?sort=score&desc=&query=database:(type:embl%20$without_version)&fil=&force=no&format=list"
-  curl $url 2> /dev/null
-done
