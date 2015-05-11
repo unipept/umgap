41952dc6c79cf50f427fd89a2ad1deee8adc05d5
Tom Naessens <Tom.Naessens@UGent.be>
2015-05-11 23:49:07 +0200

    Vervloekte fasta headers

--- tree-of-life/commonpeptfilter.py
+++ tree-of-life/commonpeptfilter.py
@@ -17,11 +17,23 @@ lines = ((line.strip().split(',')) for line in open(sys.argv[1]))
 peptides_to_filter = { line[1]: line for line in lines if line[4] in ranks_to_filter }
 
 with open(sys.argv[2], 'w') as f:
-    for pept in sys.stdin:
-        pept = pept.strip()
 
-        if pept not in peptides_to_filter:
-            print(pept)
+    header = next(sys.stdin).strip()
+    print(header)
+
+    for line in sys.stdin:
+        line = line.strip()
+
+        # Fasta mode engaged
+        if line.startswith('>'):
+            header = line
+            print(header)
         else:
-            line = peptides_to_filter[pept]
-            f.write("{},{},,{}\n".format(line[0], line[1], line[2]))
+
+            pept = line
+
+            if pept not in peptides_to_filter:
+                print(pept)
+            else:
+                line = peptides_to_filter[pept]
+                f.write("{},{},,{}\n".format(header, line[1], line[2]))
