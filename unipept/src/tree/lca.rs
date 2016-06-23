efc10b4f8a7df6b513953202d01725a7f8c4ca11
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-06-23 14:53:50 +0200

    heavy refactoring, breaks all the tests

--- unipept/src/tree/lca.rs
+++ unipept/src/tree/lca.rs
@@ -2,34 +2,27 @@
 use std::ops::Add;
 
 use agg::{Aggregator, count};
-use taxon;
 use taxon::{Taxon, TaxonId};
 use tree::tree::SubTree;
 
 pub struct LCACalculator {
     pub root: TaxonId,
-    pub taxons: Vec<Option<Taxon>>,
-    pub snapping: Vec<Option<TaxonId>>,
+    pub parents: Vec<Option<TaxonId>>,
 }
 
 impl LCACalculator {
-    pub fn new(taxons: Vec<Taxon>, ranked_only: bool) -> Self {
-        let tree     = taxon::TaxonTree::new(&taxons);
-        let by_id    = taxon::taxa_vector_by_id(taxons);
-        let snapping = tree.snapping(&by_id, ranked_only);
+    pub fn new(root: TaxonId, taxonomy: &Vec<Option<Taxon>>) -> Self {
         LCACalculator {
-            root:     tree.root,
-            taxons:   by_id,
-            snapping: snapping,
+            root:    root,
+            parents: taxonomy.iter().map(|mt| mt.as_ref().map(|t| t.parent)).collect()
         }
     }
 }
 
 impl Aggregator for LCACalculator {
-    fn aggregate(&self, taxons: &Vec<TaxonId>) -> &Taxon {
+    fn aggregate(&self, taxons: &Vec<TaxonId>) -> TaxonId {
         let counts  = count(taxons);
-        let subtree = SubTree::new(self.root, &self.taxons, counts).unwrap().collapse(&Add::add);
-        let lca     = self.snapping[subtree.root].expect("LCA does not exist");
-        self.taxons[lca].as_ref().expect("No LCA* found.")
+        let subtree = SubTree::new(self.root, &self.parents, counts).unwrap().collapse(&Add::add);
+        subtree.root
     }
 }
