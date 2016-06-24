ef1033d1a3d061e786a3c4a3478201ec12a31447
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-06-24 10:53:51 +0200

    test what a single taxon aggregates to

--- unipept/src/agg.rs
+++ unipept/src/agg.rs
@@ -20,30 +20,39 @@ mod tests {
     extern crate num_rational;
     use self::num_rational::Ratio;
     use super::*;
+    use taxon::Taxon;
     use fixtures;
     use rmq;
     use tree;
 
+    fn aggregators(by_id: &Vec<Option<Taxon>>) -> Vec<Box<Aggregator>> { vec![
+        Box::new(rmq::lca::LCACalculator::new(fixtures::tree())),
+        Box::new(rmq::rtl::RTLCalculator::new(by_id)),
+        Box::new(rmq::mix::MixCalculator::new(fixtures::tree(), Ratio::new(0, 1))),
+        Box::new(rmq::mix::MixCalculator::new(fixtures::tree(), Ratio::new(1, 1))),
+        Box::new(rmq::mix::MixCalculator::new(fixtures::tree(), Ratio::new(1, 2))),
+        Box::new(tree::lca::LCACalculator::new(fixtures::tree().root, by_id)),
+        Box::new(tree::mix::MixCalculator::new(fixtures::tree().root, by_id, Ratio::new(0, 1))),
+        Box::new(tree::mix::MixCalculator::new(fixtures::tree().root, by_id, Ratio::new(1, 1))),
+        Box::new(tree::mix::MixCalculator::new(fixtures::tree().root, by_id, Ratio::new(1, 2))),
+    ] }
 
     #[test]
     fn test_empty_query() {
-        let by_id = fixtures::by_id();
-        let aggregators: Vec<Box<Aggregator>> = vec![
-            Box::new(rmq::lca::LCACalculator::new(fixtures::tree())),
-            Box::new(rmq::rtl::RTLCalculator::new(&by_id)),
-            Box::new(rmq::mix::MixCalculator::new(fixtures::tree(), Ratio::new(0, 1))),
-            Box::new(rmq::mix::MixCalculator::new(fixtures::tree(), Ratio::new(1, 1))),
-            Box::new(rmq::mix::MixCalculator::new(fixtures::tree(), Ratio::new(1, 2))),
-            Box::new(tree::lca::LCACalculator::new(fixtures::tree().root, &by_id)),
-            Box::new(tree::mix::MixCalculator::new(fixtures::tree().root, &by_id, Ratio::new(0, 1))),
-            Box::new(tree::mix::MixCalculator::new(fixtures::tree().root, &by_id, Ratio::new(1, 1))),
-            Box::new(tree::mix::MixCalculator::new(fixtures::tree().root, &by_id, Ratio::new(1, 2))),
-        ];
-        for aggregator in aggregators {
+        for aggregator in aggregators(&fixtures::by_id()) {
             assert_eq!(
                 Err("Aggregation called on empty list.".to_owned()),
                 aggregator.aggregate(&Vec::new())
             );
+        }
+    }
+
+    #[test]
+    fn test_singleton_is_singleton() {
+        for aggregator in aggregators(&fixtures::by_id()) {
+            for taxon in fixtures::taxon_list() {
+                assert_eq!(Ok(taxon.id), aggregator.aggregate(&vec![taxon.id]));
+            }
         }
     }
 }
