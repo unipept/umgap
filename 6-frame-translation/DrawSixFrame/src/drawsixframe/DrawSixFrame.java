1babc431da2c6d7a1fd20bd4a8522cf422ba75f2
Aranka-S <aranka_steyaert@hotmail.com>
2016-11-12 11:05:06 +0100

    begin gelegd aan een print waarbij organsime ongekend moet zijn

--- 6-frame-translation/DrawSixFrame/src/drawsixframe/DrawSixFrame.java
+++ 6-frame-translation/DrawSixFrame/src/drawsixframe/DrawSixFrame.java
@@ -26,6 +26,98 @@ public class DrawSixFrame {
     private static final double pagewidth = (double) 24;
     private static final ArrayList<String> proteinSeq = new ArrayList<>();
     
+    
+    public static void organismUnknownPrint(String lcaFile, String sixframeFile){
+        System.out.println("\\begin{tikzpicture}\n" +
+        "[protein/.style={violet, line width = 6pt, line cap = round},\n" +
+        "root/.style={teal!20, line width = 4pt, line cap = round},\n" +
+        "genus/.style={teal, line width = 4pt, line cap = round},\n" +
+        "species/.style={teal!80!black, line width = 4pt, line cap = round},\n" +
+        "species group/.style={teal!90!black, line width = 4pt, line cap = round},\n" +
+        "family/.style={teal!80, line width = 4pt, line cap = round},\n" +
+        "order/.style={teal!70, line width = 4pt, line cap = round},\n" +
+        "class/.style={teal!60, line width = 4pt, line cap = round},\n" +
+        "phylum/.style={teal!50, line width = 4pt, line cap = round},\n" +
+        "kingdom/.style={teal!40, line width = 4pt, line cap = round},\n" +
+        "superkingdom/.style={teal!30, line width = 4pt, line cap = round}]\n" +
+        "\\draw node[anchor=north] {True sequence} (0,0) -- (24,0) ;\n" +
+        "\\draw (0,-2) -- (24,-2) node[anchor=south] {$+1$};\n" +
+        "\\draw (0,-3) -- (24,-3) node[anchor=south] {$+2$};\n" +
+        "\\draw (0,-4) -- (24,-4) node[anchor=south] {$+3$};\n" +
+        "\\draw (0,-5) -- (24,-5) node[anchor=south] {$-1$};\n" +
+        "\\draw (0,-6) -- (24,-6) node[anchor=south] {$-2$};\n" +
+        "\\draw (0,-7) -- (24,-7) node[anchor=south] {$-3$};");
+        CSVReader reader;
+        try {
+            reader = new CSVReader(new FileReader(lcaFile));
+            BufferedReader sixframe = new BufferedReader(new FileReader(sixframeFile));
+            List<String[]> lcas = reader.readAll();
+            Iterator<String[]> it = lcas.iterator();
+            it.next();
+            String header;
+            int framenr = 1;
+            String[] nextline = it.next();
+            String frame = "";
+            double unit = 1;
+            while((header=sixframe.readLine())!=null){
+                framenr+=1;
+                frame = sixframe.readLine();
+                unit = pagewidth/(double)frame.length();
+                for(String seq: proteinSeq){
+                    if(frame.contains(seq)){
+                        double proteinstart;
+                        double proteinstop;
+                        if(framenr>=2 && framenr <5){
+                            proteinstart = (double) frame.indexOf(seq)*unit;
+                            proteinstop = proteinstart + (double) seq.length()*unit;
+                        }else{
+                            proteinstop = (double)frame.length()*unit-(double)frame.indexOf(seq)*unit;
+                            proteinstart = proteinstop - (double) seq.length()*unit;
+                        }
+                    System.out.println("\\draw[protein] ("+proteinstart+",0) -- ("+proteinstop+",0);");
+                    }
+                }
+                boolean sameHeader = nextline[0].equals(header);
+                while(it.hasNext() && sameHeader){
+                    if(framenr>=2 && framenr <5){
+                        double start = (double) frame.indexOf(nextline[1])*unit;
+                        double end = start + (double) nextline[1].length()*unit;
+                        String taxonName = nextline[3];
+                        if(taxonName.equalsIgnoreCase("root")){
+                            System.out.println("\\draw[root] ("+start+",-"+framenr+") -- ("+end+",-"+framenr+");");
+                        }else{
+                            System.out.println("\\draw["+nextline[4]+"] ("+start+",-"+framenr+") -- ("+end+",-"+framenr+");");
+                        // Nog een node met Taxonnaam?
+                        }
+                    }else{
+                        double end = (double)frame.length()*unit-(double)frame.indexOf(nextline[1])*unit;
+                        double start = end - (double) nextline[1].length()*unit;
+                        String taxonName = nextline[3];
+                        if(taxonName.equalsIgnoreCase("root")){
+                            System.out.println("\\draw[root] ("+start+",-"+framenr+") -- ("+end+",-"+framenr+");");
+                        }else{
+                            System.out.println("\\draw["+nextline[4]+"] ("+start+",-"+framenr+") -- ("+end+",-"+framenr+");");
+                            // Nog een node met Taxonnaam?
+                        }
+                    }
+                    nextline = it.next();
+                    sameHeader = nextline[0].equals(header);
+                }
+            }
+            double end = (double)frame.length()*unit-(double)frame.indexOf(nextline[1])*unit;
+            double start = end - (double) nextline[1].length()*unit;
+            String taxonName = nextline[3];
+            if(taxonName.equalsIgnoreCase("root")){
+                System.out.println("\\draw[root] ("+start+",-"+framenr+") -- ("+end+",-"+framenr+");");
+            }else{
+                System.out.println("\\draw["+nextline[4]+"] ("+start+",-"+framenr+") -- ("+end+",-"+framenr+");");
+            // Nog een node met Taxonnaam?
+            }
+            System.out.println("\\end{tikzpicture}");
+            } catch (IOException ex) {
+            System.out.println("File found_lcas.txt not found");
+        }
+    }
     /**
      * @param args the command line arguments
      */
