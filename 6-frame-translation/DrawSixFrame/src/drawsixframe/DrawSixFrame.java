82f8b995c2cd7137f911fe6df6ea477dbf8163d0
Aranka-S <aranka_steyaert@hotmail.com>
2016-12-20 18:16:56 +0100

    tekening voor kmeren

--- 6-frame-translation/DrawSixFrame/src/drawsixframe/DrawSixFrame.java
+++ 6-frame-translation/DrawSixFrame/src/drawsixframe/DrawSixFrame.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
---
+++ 6-frame-translation/ScoreReads/src/scorereads/Kmer.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scorereads;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author Aranka
+ */
+public class Kmer {
+    public String aminoSeq;
+    public String taxonName;
+    public int taxonID;
+    public String taxonRank;
+    public int k;
+    private double score;
+    private Map<String,Double> taxonomy_score = new TreeMap<>();
+    
+    public Kmer(String seq, String taxonName, int taxonID, String taxonRank, int k, Map<String,Double> taxScore){
+        this.aminoSeq = seq;
+        this.taxonID = taxonID;
+        this.taxonName = taxonName;
+        this.taxonRank = taxonRank;
+        this.k = k;
+        this.taxonomy_score = taxScore;
+        calculateScore();
+    }
+    
+    public Kmer(String Unipeptinfo, int k, Map<String,Double> taxScore){
+        String[] info = Unipeptinfo.split(",");
+        this.aminoSeq = info[0];
+        this.taxonID = Integer.parseInt(info[1]);
+        this.taxonName = info[2];
+        this.taxonRank = info[3];
+        this.k = k;
+        this.taxonomy_score = taxScore;
+        calculateScore();
+    }
+    
+    private void calculateScore(){
+        this.score = 0;
+        // Nog te implementeren
+    }
+    
+//      Niet meer nodig, vooraf berekenen via unipept cli    
+//    private String[] getTaxonInfo(int taxonID){
+//        String[] info = new String[2];
+//        String url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=taxonomy&id=" + taxonID;
+//        try {
+//            URL taxonomyPage = new URL(url);
+//        } catch (MalformedURLException ex) {
+//            System.out.println("Url to retreive taxonomy could nog be followed");
+//            System.out.println("Given url: " + url);
+//        }
+//        return info;
+//    } 
+}
