fefc6b5180d93e8022aaa4de75a8ca55c2cddb36
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-01-15 23:00:20 +0100

    add option for more taxon output

--- unipept/src/bin/pept2lca.rs
+++ unipept/src/bin/pept2lca.rs
@@ -11,7 +11,7 @@ extern crate fst;
 use fst::Map;
 
 extern crate unipept;
-use unipept::{PKG_NAME, PKG_VERSION, PKG_AUTHORS};
+use unipept::{PKG_NAME, PKG_VERSION, PKG_AUTHORS, taxon};
 use unipept::errors::Result;
 
 const ABOUT: &'static str = "
@@ -25,26 +25,40 @@ fn main() {
         .version(PKG_VERSION)
         .author(PKG_AUTHORS.split(':').next().unwrap_or("unknown"))
         .about(ABOUT)
+        .arg(Arg::with_name("taxon-file")
+            .help("The NCBI taxonomy tsv-file")
+            .short("t")
+            .long("taxa")
+            .takes_value(true))
         .arg(Arg::with_name("FST index")
             .help("An FST to query")
             .required(true))
         .get_matches();
-    main_result(matches.value_of("FST index").unwrap() /* required so safe */)
-        .unwrap_or_else(|err| {
-            writeln!(&mut io::stderr(), "{}", err).unwrap();
-            process::exit(1);
-        });
+    main_result(
+        matches.value_of("FST index").unwrap(), // required so safe
+        matches.value_of("taxon-file"),
+    ).unwrap_or_else(|err| {
+        writeln!(&mut io::stderr(), "{}", err).unwrap();
+        process::exit(1);
+    });
 }
 
-fn main_result(fst: &str) -> Result<()> {
+fn main_result(fst: &str, taxons: Option<&str>) -> Result<()> {
     let fst = try!(Map::from_path(fst));
+    let by_id = taxons.map(|taxons| taxon::read_taxa_file(taxons).map_err(|err| err.to_string()).unwrap())
+                      .map(|taxons| taxon::taxa_vector_by_id(taxons));
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = try!(line);
         if line.starts_with('>') {
             println!("{}", line);
         } else if let Some(lca) = fst.get(line) {
-            println!("{}", lca);
+            if let Some(ref by_id) = by_id {
+                let taxon = by_id[lca as usize].as_ref().unwrap();
+                println!("{},{},{}", taxon.id, taxon.name, taxon.rank);
+            } else {
+                println!("{}", lca);
+            }
         }
     }
     Ok(())
