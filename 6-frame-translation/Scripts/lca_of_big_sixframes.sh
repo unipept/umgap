463fb34e18c3dd71dd26d22b6c7a01bb21edf701
Aranka Steyaert <aranka_steyaert@hotmail.com>
2017-06-01 16:34:50 +0200

    remove unnecessary files

--- 6-frame-translation/Scripts/lca_of_big_sixframes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sixframe=$1.sixframe
-cat $sixframe | prot2pept | peptfilter > temp
-lines=$(wc -l temp | sed 's/^[ ]*//' | cut -d " " -f 1)
-for i in $(seq 500 500 8062018) 
-do
-	head -n $i temp | tail -n 500 | unipept pept2lca >> $1.lcas.txt
-done
