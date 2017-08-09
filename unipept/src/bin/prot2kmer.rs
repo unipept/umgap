a999bc7aa2de08492f00bd8f6205049c7875c9b4
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-08-09 12:01:16 +0200

    using windows should be faster

--- unipept/src/bin/prot2kmer.rs
+++ unipept/src/bin/prot2kmer.rs
@@ -2,6 +2,7 @@
 use std::process;
 use std::io;
 use std::io::Write;
+use std::borrow::Cow;
 
 extern crate clap;
 use clap::{Arg, App};
@@ -38,14 +39,13 @@ fn main_result(k: &str) -> Result<()> {
 
     let mut writer = fasta::Writer::new(io::stdout(), "\n", false);
     for record in fasta::Reader::new(io::stdin(), None, true).records() {
-        let record = try!(record);
-        if record.sequence[0].len() < k { continue }
-        let new = (0..record.sequence[0].len() - k + 1)
-            .map(|i| record.sequence[0][i..i + k].to_string())
-            .collect::<Vec<_>>();
+        let fasta::Record { header, sequence } = try!(record);
+        if sequence[0].len() < k { continue }
         try!(writer.write_record(fasta::Record {
-            header: record.header,
-            sequence: new,
+            header: header,
+            sequence: sequence[0].as_bytes().windows(k)
+                                 .map(String::from_utf8_lossy).map(Cow::into_owned)
+                                 .collect(),
         }));
     }
     Ok(())
