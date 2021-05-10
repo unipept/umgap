9e147726734b1204ca49b62383293d8d88b93c73
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2021-05-10 22:52:49 +0200

    allow comparative counts with taxa2freq

--- src/commands/taxa2freq.rs
+++ src/commands/taxa2freq.rs
@@ -1,8 +1,10 @@
 //! The `umgap taxa2freq` command.
 
 use std::collections::HashMap;
+use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Write;
 use std::path::PathBuf;
 
@@ -67,6 +69,10 @@ pub struct TaxaToFreq {
     /// An NCBI taxonomy TSV-file as processed by Unipept
     #[structopt(parse(from_os_str))]
     pub taxon_file: PathBuf,
+
+    /// Multiple comparative input files.
+    #[structopt(parse(from_os_str))]
+    pub input_files: Vec<PathBuf>,
 }
 
 /// Implements the taxa2freq command.
@@ -75,6 +81,7 @@ pub fn taxa2freq(args: TaxaToFreq) -> errors::Result<()> {
     if args.rank == rank::Rank::NoRank {
         return Err(errors::ErrorKind::InvalidInvocation("Snap to an actual rank.".into()).into());
     }
+    let numfiles = args.input_files.len();
 
     // Parsing the taxons
     let tree = taxon::TaxonTree::new(&taxons);
@@ -82,29 +89,71 @@ pub fn taxa2freq(args: TaxaToFreq) -> errors::Result<()> {
     let snapping =
         tree.filter_ancestors(|tid| by_id.get(tid).map(|t| t.rank == args.rank).unwrap_or(false));
 
-    // Read and count taxon ranks
-    let mut counts = HashMap::new();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let taxon = line?.parse::<taxon::TaxonId>()?;
-        *counts.entry(snapping[taxon].unwrap_or(0)).or_insert(0) += 1;
-    }
-
-    let mut counts = counts
-        .into_iter()
-        .filter(|&(_taxon, count)| count > args.min_frequency)
-        .map(|(taxon, count)| (count, taxon))
-        .collect::<Vec<(taxon::TaxonId, usize)>>();
-    counts.sort();
-
+    // Grab stdout
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
-    for (count, taxon) in counts {
-        let taxon = by_id
-            .get(taxon)
-            .ok_or("LCA taxon id not in taxon list. Check compatibility with index.")?;
-        writeln!(stdout, "{}	{}	{}", count, taxon.id, taxon.name)?;
+
+    // Writing headers
+    write!(stdout, "taxon id,taxon name")?;
+    if numfiles == 0 {
+        write!(stdout, ",stdin")?;
+    } else {
+        for filename in args.input_files.iter() {
+            write!(stdout, ",{}", filename.to_string_lossy())?;
+        }
+    }
+    writeln!(stdout)?;
+
+    // Read and count taxon ranks
+    let mut counts = HashMap::new();
+    if numfiles == 0 {
+        let stdin = io::stdin();
+        count_file(&snapping, &mut counts, 0, 1, Box::new(stdin.lock()))?;
+    } else {
+        for (i, file) in args.input_files.iter().enumerate() {
+            count_file(
+                &snapping,
+                &mut counts,
+                i,
+                numfiles,
+                Box::new(BufReader::new(File::open(file)?)),
+            )?;
+        }
     }
 
+    // Print rows
+    for (tid, row) in counts {
+        let taxon = by_id
+            .get(tid)
+            .ok_or("LCA taxon id not in taxon list. Check compatibility with index.")?;
+        if row.iter().sum::<usize>() > args.min_frequency {
+            write!(stdout, "{},{}", taxon.id, taxon.name)?;
+            for count in row {
+                write!(stdout, ",{}", count)?;
+            }
+            writeln!(stdout)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn count_file<T: BufRead>(
+    snapping: &Vec<Option<taxon::TaxonId>>,
+    counts: &mut HashMap<taxon::TaxonId, Vec<usize>>,
+    index: usize,
+    numfiles: usize,
+    file: T,
+) -> errors::Result<()> {
+    for line in file.lines() {
+        match line?.parse::<taxon::TaxonId>() {
+            Ok(taxon) => {
+                counts
+                    .entry(snapping[taxon].unwrap_or(0))
+                    .or_insert(vec![0; numfiles])[index] += 1
+            }
+            Err(_) => (),
+        }
+    }
     Ok(())
 }
